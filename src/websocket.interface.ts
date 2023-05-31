d9034d96a5e084171fb350d9e011fc7f265f2112
ryinner <vinograd18062002@gmail.com>
2023-05-31 13:59:32 +0500

    remove unused interface

--- src/websocket.interface.ts
+++ src/websocket.interface.ts
@@ -13,22 +13,3 @@ export interface WebSocketOperation {
 export type WebSocketAnswerData = Record<string, unknown>;
 export type WebSocketCallback = (webSocket: WebSocket) => void;
 export type WebSocketMessageHandlerCallback = (data: WebSocketAnswerData) => void;
-
-export interface SessionUser {
-    city_id: string;
-    city_name: string;
-    ip: string;
-    open: boolean;
-    sent: boolean;
-    repeated: boolean;
-    product_id: string;
-    url: string;
-    title: string;
-    roistat: string;
-    uid: string;
-    um_id?: string;
-    ym_uid: string;
-    username?: string;
-}
-
-export type SessionAnswer = Record<string, SessionUser>;
